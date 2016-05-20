1471df969832e0d9ca47ce08f590dae2048f2a99
miguel12340 <miguelrochacalhau@hotmail.com>
2016-05-20 11:14:59 +0100

    new design

--- Proj/Cliente/Proj_Seguranca/Form1.Designer.cs
+++ Proj/Cliente/Proj_Seguranca/Form1.Designer.cs
@@ -31,10 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.txtChavePrivada = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblStatus = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnEnviarFicheiro = new System.Windows.Forms.Button();
-            this.txtFicheiro = new System.Windows.Forms.TextBox();
             this.lbFicheiros = new System.Windows.Forms.ListBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -45,17 +43,23 @@
             this.txtChavePublica = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.panel_Status = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 2);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(89, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 22);
+            this.button1.Size = new System.Drawing.Size(159, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "Conectar ao servidor";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,105 +67,91 @@
             // 
             // txtChavePrivada
             // 
-            this.txtChavePrivada.Location = new System.Drawing.Point(8, 18);
-            this.txtChavePrivada.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtChavePrivada.Enabled = false;
+            this.txtChavePrivada.Location = new System.Drawing.Point(11, 22);
+            this.txtChavePrivada.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtChavePrivada.Multiline = true;
             this.txtChavePrivada.Name = "txtChavePrivada";
-            this.txtChavePrivada.Size = new System.Drawing.Size(288, 77);
+            this.txtChavePrivada.Size = new System.Drawing.Size(383, 94);
             this.txtChavePrivada.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 6);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(8, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Status:";
             // 
-            // lblStatus
-            // 
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(47, 6);
-            this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(16, 13);
-            this.lblStatus.TabIndex = 4;
-            this.lblStatus.Text = "...";
-            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel_Status);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.lblStatus);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(455, 10);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(232, 11);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(224, 28);
+            this.panel1.Size = new System.Drawing.Size(567, 34);
             this.panel1.TabIndex = 5;
             // 
             // btnEnviarFicheiro
             // 
-            this.btnEnviarFicheiro.Location = new System.Drawing.Point(586, 308);
-            this.btnEnviarFicheiro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEnviarFicheiro.Location = new System.Drawing.Point(201, 271);
+            this.btnEnviarFicheiro.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnEnviarFicheiro.Name = "btnEnviarFicheiro";
-            this.btnEnviarFicheiro.Size = new System.Drawing.Size(88, 22);
+            this.btnEnviarFicheiro.Size = new System.Drawing.Size(113, 27);
             this.btnEnviarFicheiro.TabIndex = 8;
             this.btnEnviarFicheiro.Text = "Enviar Ficheiro";
             this.btnEnviarFicheiro.UseVisualStyleBackColor = true;
-            // 
-            // txtFicheiro
-            // 
-            this.txtFicheiro.Location = new System.Drawing.Point(228, 308);
-            this.txtFicheiro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtFicheiro.Multiline = true;
-            this.txtFicheiro.Name = "txtFicheiro";
-            this.txtFicheiro.Size = new System.Drawing.Size(355, 93);
-            this.txtFicheiro.TabIndex = 9;
             // 
             // lbFicheiros
             // 
             this.lbFicheiros.FormattingEnabled = true;
             this.lbFicheiros.ItemHeight = 16;
-            this.lbFicheiros.Location = new System.Drawing.Point(15, 42);
-            this.lbFicheiros.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbFicheiros.Location = new System.Drawing.Point(10, 38);
+            this.lbFicheiros.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.lbFicheiros.Name = "lbFicheiros";
-            this.lbFicheiros.Size = new System.Drawing.Size(113, 251);
+            this.lbFicheiros.Size = new System.Drawing.Size(304, 228);
             this.lbFicheiros.TabIndex = 10;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(705, 50);
-            this.txtNome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNome.Location = new System.Drawing.Point(315, 7);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(123, 20);
+            this.txtNome.Size = new System.Drawing.Size(163, 22);
             this.txtNome.TabIndex = 11;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(650, 53);
+            this.label2.Location = new System.Drawing.Point(258, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(49, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "Nome:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(874, 53);
+            this.label3.Location = new System.Drawing.Point(486, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(49, 17);
             this.label3.TabIndex = 12;
             this.label3.Text = "ID: #...";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(221, 9);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(34, 63);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(182, 22);
+            this.button2.Size = new System.Drawing.Size(243, 27);
             this.button2.TabIndex = 13;
             this.button2.Text = "Gerar Chaves (Privada /  Publica)";
             this.button2.UseVisualStyleBackColor = true;
@@ -170,82 +160,127 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txtChavePrivada);
-            this.groupBox1.Location = new System.Drawing.Point(210, 42);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Location = new System.Drawing.Point(19, 104);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(302, 108);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(403, 133);
             this.groupBox1.TabIndex = 14;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chave Privada";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.txtChavePublica);
-            this.groupBox2.Location = new System.Drawing.Point(210, 181);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Location = new System.Drawing.Point(19, 247);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox2.Size = new System.Drawing.Size(302, 108);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(403, 133);
             this.groupBox2.TabIndex = 15;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Chave Publica";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
             // txtChavePublica
             // 
-            this.txtChavePublica.Location = new System.Drawing.Point(4, 20);
-            this.txtChavePublica.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtChavePublica.Location = new System.Drawing.Point(5, 25);
+            this.txtChavePublica.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtChavePublica.Multiline = true;
             this.txtChavePublica.Name = "txtChavePublica";
-            this.txtChavePublica.Size = new System.Drawing.Size(288, 77);
+            this.txtChavePublica.Size = new System.Drawing.Size(383, 94);
             this.txtChavePublica.TabIndex = 2;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(619, 233);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(19, 384);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(157, 48);
+            this.button3.Size = new System.Drawing.Size(149, 59);
             this.button3.TabIndex = 16;
             this.button3.Text = "Enviar chave publica para o servidor";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 19);
+            this.label4.Location = new System.Drawing.Point(7, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(184, 17);
             this.label4.TabIndex = 17;
             this.label4.Text = "Lista de Ficheiros enviados:";
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 271);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 27);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Escolher Ficheiro ...";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.lbFicheiros);
+            this.groupBox3.Controls.Add(this.btnEnviarFicheiro);
+            this.groupBox3.Location = new System.Drawing.Point(475, 63);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(324, 303);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ficheiros";
+            // 
+            // panel_Status
+            // 
+            this.panel_Status.Location = new System.Drawing.Point(66, 10);
+            this.panel_Status.Name = "panel_Status";
+            this.panel_Status.Size = new System.Drawing.Size(17, 17);
+            this.panel_Status.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 39);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Client Side";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 527);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(811, 463);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtNome);
-            this.Controls.Add(this.lbFicheiros);
-            this.Controls.Add(this.txtFicheiro);
-            this.Controls.Add(this.btnEnviarFicheiro);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -256,10 +291,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtChavePrivada;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnEnviarFicheiro;
-        private System.Windows.Forms.TextBox txtFicheiro;
         private System.Windows.Forms.ListBox lbFicheiros;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label label2;
@@ -270,6 +303,11 @@
         private System.Windows.Forms.TextBox txtChavePublica;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Panel panel_Status;
+        private System.Windows.Forms.Label label5;
     }
 }
 
