b6584937b59b08630a0705c42b208570654d05ca
roudry2 <roudry2@hotmail.com>
2016-05-31 09:14:20 +0100

    aceite

--- Proj/Cliente/Proj_Seguranca/Form1.cs
+++ Proj/Cliente/Proj_Seguranca/Form1.cs
@@ -1,17 +1,9 @@
-﻿using EI.SI;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Proj_Seguranca
